b9da4a3eb4610d1c32b2df17b562bc6570186595
svnblame <gene@bizflowdesigns.com>
2014-06-10 10:17:55 -0500

    Added array functions code, Locaton 11429

--- day_mon2.c
+++ day_mon2.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 int main(void)
 {
-   const int days[] = {31,28,31,30,31,30,31,31,30,31};
+   const int days[] = {31,28,31,30,31,30,31,31,30,31,30,31};
    int index;
    
    for(index = 0; index < sizeof days / sizeof days[0]; index++)
--- rain.c
+++ rain.c
@@ -11,7 +11,7 @@ int main(void)
 		{4.3,4.3,4.3,3.0,2.0,1.2,0.2,0.2,0.4,2.4,3.5,6.6},
 		{8.5,8.2,1.2,1.6,2.4,0.0,5.2,0.9,0.3,0.9,1.4,7.3},
 		{9.1,8.5,6.7,4.3,2.1,0.8,0.2,0.2,1.1,2.3,6.1,8.4},
-		{7.2,9.9,8.4,3.3,2.1,0.8,0.2,0.2,1.1,2.3,6.1,8.4},
+		{7.2,9.9,8.4,3.3,1.2,0.8,0.4,0.0,0.6,1.7,4.3,6.2},
 		{7.6,5.6,3.8,2.8,3.8,0.2,0.0,0.0,0.0,1.3,2.6,5.2}
 	};
 	int year, month;
@@ -20,7 +20,8 @@ int main(void)
 	printf(" YEAR    RAINFALL   (inches)\n");
 	for (year = 0, total = 0; year < YEARS; year++)
 	{ // for each year, sum rainfall for each month
-		subtot += rain[year][month];
+		for (month = 0, subtot = 0; month < MONTHS; month++)
+			subtot += rain[year][month];
 		printf("%5d %15.1f\n", 2010 + year, subtot);
 		total += subtot; // total for all years
 	}
@@ -39,4 +40,3 @@ int main(void)
 	
 	return 0;
 }
-
